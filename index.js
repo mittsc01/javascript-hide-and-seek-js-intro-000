020e1dcdf056c6408bc46a83c25b4213ecb676a5
Scott Mittman <sm2884@nau.edu>
2020-06-14 16:00:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+function increaseRankBy(n){
+  const rList=document.querySelectorAll(".ranked-list li")
+  for (let i=0;i<rList.length;i++){
+    rList[i].innerHTML=parseInt(rList[i].innerHTML)+n
+  }
+}
+function deepestChild(){
+  return document.querySelector("#grand-node div div div div")
+}
