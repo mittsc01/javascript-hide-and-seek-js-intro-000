1fcd93ff84a7187d6aea6b288dd3fbb405ca38d2
Scott Mittman <sm2884@nau.edu>
2020-06-14 15:55:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget(){
   return document.querySelector("#nested .target")
 }
 function increaseRankBy(n){
-  const rList=document.querySelectorAll(".ranked-list li")
+  const rList=document.getElementById('app').querySelectorAll(".ranked-list li")
   for (let i=0;i<rList.length;i++){
     rList[i].innerHTML=parseInt(rList.innerHTML)+n
   }
